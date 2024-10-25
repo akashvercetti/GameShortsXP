d28b0a9d836ae165bf569338cf61566d17c080da
Akash Malhotra <carljohnson.akash@gmail.com>
2024-10-25 19:53:18 +0530

    hashrouter update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import logo from '../assets/icon.png'; // Adjust this path to your logo image
 import googlePlayBadge from '../assets/google-play-badge.png'; // Path to your Google Play badge
 
@@ -8,11 +8,12 @@ const Header = () => {
   return (
     <header className="header">
       <Link to="/">
-      <img src={logo} alt="App Logo" className="logo" />
+        <img src={logo} alt="App Logo" className="logo" />
       </Link>
+      <h1 className="app-title">GameShortsXP</h1> {/* Centered app title */}
       <a href="https://play.google.com/store/apps/details?id=com.akashvercetti.gameshortsxp">
         <img
-          src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+          src={googlePlayBadge}
           alt="Get it on Google Play"
           className="google-play-badge"
         />
